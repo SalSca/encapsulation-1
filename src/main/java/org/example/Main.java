b9f957703e3dc5d218b98c4764096c3645149640
Salvatore Scalisi <scalisi.salvo02@gmail.com>
2022-12-10 21:34:54 +0100

    commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        House house = new House();
+
+        //----------
+        System.out.println("Write the address of the house: ");
+        house.setAddress(scanner.nextLine());
+        //----------
+        System.out.println("Write the number of floors here: ");
+        house.setFloorsNumber(scanner.nextInt());
+        //----------
+        System.out.println("Write the names of the residents: ");
+        house.setResidentsNames(scanner.next().split(","));
+        //----------
+        System.out.println(house.getAddress()+" "+house.getFloorsNumber());
+        System.out.println("-----------");
+        System.out.println(Arrays.toString(house.getResidentsNames()));
+    }
+}
